ffbcea50a15b2458ab22151bf6ea180583a2b617
Gian Marino <gcmarino99@gmail.com>
2026-05-12 01:46:43 -0400

    feat: docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
+      queue: process.env.RABBITMQ_QUEUE ?? 'sgv_queue',
+      queueOptions: {
+        durable: true
+      }
+    }
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
